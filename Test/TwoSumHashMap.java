cd4d5e02349f6f3251034f6a8fc9ea5ef754e2ee
jing1522 <3038254848@qq.com>
2026-03-28 18:55:21 +0800

    Add files via upload

---
+++ Test/TwoSumHashMap.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+
+
+public class TwoSumHashMap {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the target value:");
+        int target=sc.nextInt();
+        System.out.println("Enter the number of elements:");
+        int n=sc.nextInt();
+        int[] nums =new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the number,one by one:");
+            nums[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+           if(map.containsKey(target-nums[i])){
+            System.out.println("Indices: "+map.get(target-nums[i])+" and "+i);
+            return;
+           }
+           else{
+            map.put(nums[i],i);
+           }
+          
+        }
+        System.out.println("No solution found.");
+       
+        sc.close();
+    }
+
+    
+}
