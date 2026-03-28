eae401f48abd2408b6e208c2a53cb6cd90703178
jing1522 <3038254848@qq.com>
2026-03-28 10:09:42 +0800

    Add files via upload
    完成素数判断练习

---
+++ Test/PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+public class PrimeNumber {
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        boolean flag=true;
+        if(num<=1){
+            System.out.println("not a prime");
+        }
+        else{
+            for(int i =2;i<=sqrt(num);i++){
+                if(num%i==0){
+                    flag=false;
+                    break;
+                }
+
+            }
+            if(flag==true){
+                System.out.println("prime");
+            }
+            else{
+                System.out.println("not a prime");
+            }
+        }
+
+
+    }
+    
+}
