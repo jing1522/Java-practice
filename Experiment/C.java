b703ba6213793bd043ca716b74a49c537c20fe44
jing1522 <3038254848@qq.com>
2026-04-03 12:55:52 +0800

    Add files via upload

---
+++ Experiment/C.java
@@ -0,0 +1,5 @@
+public class C {
+    void fC(){
+        System.out.println("I am C");
+    }
+}
---
+++ Experiment/MainClass.java
@@ -0,0 +1,9 @@
+public class MainClass {
+    public static void main(String[] args) {
+       System.out.println("你只需要编译我");
+       A a = new A();
+       a.fA();
+       B b=new B();
+       b.fB();
+    }
+}
